0beb4451aaa12165c1da32e3e14bc19a22aec4a5
Masayoshi Mizutani <mizutani@cookpad.com>
2021-08-29 22:41:35 +0900

    Skip bundle.js test in GitHub actions

--- assets/assets_test.go
+++ assets/assets_test.go
@@ -1,6 +1,7 @@
 package assets_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/m-mizutani/alertchain/assets"
@@ -9,6 +10,7 @@ import (
 )
 
 func TestAssets(t *testing.T) {
+
 	t.Run("index.html loadable", func(t *testing.T) {
 		raw, err := assets.Assets().ReadFile("dist/index.html")
 		require.NoError(t, err)
@@ -16,6 +18,9 @@ func TestAssets(t *testing.T) {
 	})
 
 	t.Run("bundle.js loadable", func(t *testing.T) {
+		if _, ok := os.LookupEnv("GITHUB_WORKFLOW"); ok {
+			t.Skip("bundle.js is not generated in GitHub Actions")
+		}
 		raw, err := assets.Assets().ReadFile("dist/bundle.js")
 		require.NoError(t, err)
 		assert.Contains(t, string(raw), "__webpack_modules__")
