531f5d6b73bbf3f522a5bfa8490c47a4e5026566
Masayoshi Mizutani <mizutani@cookpad.com>
2021-09-04 17:53:10 +0900

    Add CLIWithChain to implement static one binary

--- pkg/controller/cli.go
+++ pkg/controller/cli.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"os"
 	"plugin"
 
 	"github.com/m-mizutani/alertchain"
@@ -17,6 +16,7 @@ import (
 
 type config struct {
 	logLevel string
+	chain    *alertchain.Chain
 
 	// serve
 	DBType     string
@@ -29,8 +29,11 @@ type config struct {
 
 var logger = utils.Logger
 
-func (x *Controller) CLI(args []string) {
-	var cfg config
+func (x *Controller) CLIWithChain(args []string, chain *alertchain.Chain) error {
+	cfg := config{
+		chain: chain,
+	}
+
 	app := cli.App{
 		Name:        "alertchain",
 		Version:     types.Version,
@@ -57,53 +60,62 @@ func (x *Controller) CLI(args []string) {
 
 	if err := app.Run(args); err != nil {
 		utils.HandleError(err)
-		os.Exit(1)
+		return err
 	}
+	return nil
+}
+
+func (x *Controller) CLI(args []string) error {
+	return x.CLIWithChain(args, nil)
 }
 
 func cmdServe(cfg *config) *cli.Command {
-	return &cli.Command{
-		Name: "serve",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:        "db-type",
-				Aliases:     []string{"t"},
-				Usage:       "database type",
-				Destination: &cfg.DBType,
-				Value:       "sqlite3",
-				EnvVars:     []string{"ALERTCHAIN_DB_TYPE"},
-			},
-			&cli.StringFlag{
-				Name:        "db-config",
-				Aliases:     []string{"d"},
-				Usage:       "database configuration",
-				Destination: &cfg.DBConfig,
-				Value:       "file:ent?mode=memory&cache=shared&_fk=1",
-				EnvVars:     []string{"ALERTCHAIN_DB_CONFIG"},
-			},
-			&cli.StringFlag{
-				Name:        "server-addr",
-				Aliases:     []string{"a"},
-				Usage:       "server binding adddress",
-				Value:       "localhost",
-				Destination: &cfg.ServerAddr,
-			},
-			&cli.Uint64Flag{
-				Name:        "server-port",
-				Aliases:     []string{"p"},
-				Usage:       "server binding port",
-				Value:       9080,
-				Destination: &cfg.ServerPort,
-			},
-
-			&cli.StringFlag{
-				Name:        "chain",
-				Aliases:     []string{"c"},
-				Usage:       "chain plugin path",
-				Required:    true,
-				Destination: &cfg.ChainPath,
-			},
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:        "db-type",
+			Aliases:     []string{"t"},
+			Usage:       "database type",
+			Destination: &cfg.DBType,
+			Value:       "sqlite3",
+			EnvVars:     []string{"ALERTCHAIN_DB_TYPE"},
 		},
+		&cli.StringFlag{
+			Name:        "db-config",
+			Aliases:     []string{"d"},
+			Usage:       "database configuration",
+			Destination: &cfg.DBConfig,
+			Value:       "file:ent?mode=memory&cache=shared&_fk=1",
+			EnvVars:     []string{"ALERTCHAIN_DB_CONFIG"},
+		},
+		&cli.StringFlag{
+			Name:        "server-addr",
+			Aliases:     []string{"a"},
+			Usage:       "server binding adddress",
+			Value:       "localhost",
+			Destination: &cfg.ServerAddr,
+		},
+		&cli.Uint64Flag{
+			Name:        "server-port",
+			Aliases:     []string{"p"},
+			Usage:       "server binding port",
+			Value:       9080,
+			Destination: &cfg.ServerPort,
+		},
+	}
+
+	if cfg.chain == nil {
+		flags = append(flags, &cli.StringFlag{
+			Name:        "chain",
+			Aliases:     []string{"c"},
+			Usage:       "chain plugin path",
+			Required:    true,
+			Destination: &cfg.ChainPath,
+		})
+	}
+
+	return &cli.Command{
+		Name:  "serve",
+		Flags: flags,
 
 		Action: func(c *cli.Context) error {
 			logger.Info().Interface("config", cfg).Msg("Starting AlertChain")
@@ -120,9 +132,13 @@ func cmdServe(cfg *config) *cli.Command {
 			}()
 
 			// Setup chain
-			chain, err := loadChainPlugin(cfg.ChainPath)
-			if err != nil {
-				return err
+			chain := cfg.chain
+			if chain == nil {
+				c, err := loadChainPlugin(cfg.ChainPath)
+				if err != nil {
+					return err
+				}
+				chain = c
 			}
 
 			// Setup usecase
